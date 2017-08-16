04acc4b59bebfd675a4ca2cf9b020d055e3d43b6
Andreas Peters <mailbox@andreas-peters.net>
2017-08-16 15:47:17 +0200

    change credentials function

--- services/aws/aws.go
+++ services/aws/aws.go
@@ -3,7 +3,6 @@ package aws
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/service/ec2"
 
@@ -23,14 +22,17 @@ const ServiceType = "aws"
 // Service represents the AWS service. It has no Config fields.
 type Service struct {
 	types.DefaultService
-	// The Users who are allowed to use the invoice service
-	AllowedUsers string
-	// AWS AccessKey
-	AccessKey string
-	// AWS Secret Key
-	SecretAccessKey string
-	// AWS Token
-	AccessToken string
+	// The Usermaping to the AWS Keys
+	Users map[string]struct {
+		// AWS AccessKey
+		AccessKey string
+		// AWS Secret Key
+		SecretAccessKey string
+		// AWS Token
+		AccessToken string
+		// AWS Region
+		Region string
+	}
 }
 
 // Commands supported:
@@ -43,7 +45,6 @@ func (s *Service) Commands(cli *gomatrix.Client) []types.Command {
 				var message string
 				message = fmt.Sprintf("##### Help \n")
 				message = message + fmt.Sprintf("```\n ")
-				message = message + fmt.Sprintf("acl\n======\n \t Give a list of all allowed users\n\n")
 				message = message + fmt.Sprintf("instance start\n======\n \t Instance Id: Start the Instance \n\n")
 				message = message + fmt.Sprintf("instance stop\n======\n \t Instance Id: Stop the Instance \n\n")
 				message = message + fmt.Sprintf("instance show\n======\n \t Give out a list of all instances \n\n")
@@ -52,39 +53,21 @@ func (s *Service) Commands(cli *gomatrix.Client) []types.Command {
 			},
 		},
 		types.Command{
-			Path: []string{"aws", "acl"},
-			Command: func(roomID, userID string, args []string) (interface{}, error) {
-				if strings.Contains(s.AllowedUsers, userID) {
-					return &gomatrix.TextMessage{"m.notice", fmt.Sprintf("Allowed Users: %s", s.AllowedUsers)}, nil
-				}
-				return &gomatrix.TextMessage{"m.notice", "U are not allowed to use this function"}, nil
-			},
-		},
-		types.Command{
 			Path: []string{"aws", "instance", "start"},
 			Command: func(roomID, userID string, args []string) (interface{}, error) {
-				if strings.Contains(s.AllowedUsers, userID) && len(args) == 1 {
-					return s.cmdAwsInstanceStart(roomID, userID, args)
-				}
-				return &gomatrix.TextMessage{"m.notice", "U are not allowed to use this function or u forgot some arguments"}, nil
+				return s.cmdAwsInstanceStart(roomID, userID, args)
 			},
 		},
 		types.Command{
 			Path: []string{"aws", "instance", "stop"},
 			Command: func(roomID, userID string, args []string) (interface{}, error) {
-				if strings.Contains(s.AllowedUsers, userID) && len(args) == 1 {
-					return s.cmdAwsInstanceStop(roomID, userID, args)
-				}
-				return &gomatrix.TextMessage{"m.notice", "U are not allowed to use this function or u forgot some arguments"}, nil
+				return s.cmdAwsInstanceStop(roomID, userID, args)
 			},
 		},
 		types.Command{
 			Path: []string{"aws", "instance", "show"},
 			Command: func(roomID, userID string, args []string) (interface{}, error) {
-				if strings.Contains(s.AllowedUsers, userID) {
-					return s.cmdAwsInstanceShow(roomID, userID, args)
-				}
-				return &gomatrix.TextMessage{"m.notice", "U are not allowed to use this function"}, nil
+				return s.cmdAwsInstanceShow(roomID, userID, args)
 			},
 		},
 	}
@@ -97,7 +80,7 @@ func (s *Service) cmdAwsInstanceStop(roomID, userID string, args []string) (inte
 	log.Info("Service: Aws: Stop Instance: ", instanceId)
 
 	// have to login first to get a aws session
-	sess := s.awsLogin()
+	sess := s.awsLogin(userID)
 
 	if sess != nil {
 		// to start a instance, we need a ec2 session
@@ -126,7 +109,7 @@ func (s *Service) cmdAwsInstanceStart(roomID, userID string, args []string) (int
 	log.Info("Service: Aws: Start Instance: ", instanceId)
 
 	// have to login first to get a aws session
-	sess := s.awsLogin()
+	sess := s.awsLogin(userID)
 
 	if sess != nil {
 		// to start a instance, we need a ec2 session
@@ -153,7 +136,7 @@ func (s *Service) cmdAwsInstanceShow(roomID, userID string, args []string) (inte
 	log.Info("Service: Aws: Show Instance")
 
 	// Have to login first
-	sess := s.awsLogin()
+	sess := s.awsLogin(userID)
 
 	if sess != nil {
 		// create me a new ecs session and get out a list of all instances
@@ -210,10 +193,24 @@ func printValue(message *string, length int) string {
 	return *message
 }
 
-func (s *Service) awsLogin() *session.Session {
+// get the aws credentials of a user
+func (s *Service) getCredentials(userID string) (string, string, string, string) {
+	for localUserID, userConfig := range s.Users {
+		if localUserID == userID {
+			return userConfig.AccessKey, userConfig.SecretAccessKey, userConfig.AccessToken, userConfig.Region
+		}
+	}
+
+	return "", "", "", ""
+}
+
+// loginto the aws
+func (s *Service) awsLogin(userID string) *session.Session {
+	AccessKey, SecretAccessKey, AccessToken, Region := s.getCredentials(userID)
+
 	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String("us-west-2"),
-		Credentials: credentials.NewStaticCredentials(s.AccessKey, s.SecretAccessKey, s.AccessToken),
+		Region:      aws.String(Region),
+		Credentials: credentials.NewStaticCredentials(AccessKey, SecretAccessKey, AccessToken),
 	})
 
 	if err != nil {
